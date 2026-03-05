3ad6e80312966b8ba0386161f35a2e49e0322e54
kaiii708 <yuvivian0708@gmail.com>
2026-03-05 17:52:35 +0800

    Increase real-time simulator hard limit to 200ms

--- examples/nr-mec-3gpp-calibration.cc
+++ examples/nr-mec-3gpp-calibration.cc
@@ -1340,8 +1340,8 @@ main(int argc, char* argv[])
         Config::SetDefault("ns3::RealtimeSimulatorImpl::SynchronizationMode",
                            StringValue("HardLimit"));
         Config::SetDefault("ns3::RealtimeSimulatorImpl::HardLimit",
-                           TimeValue(MilliSeconds(100)));
-        NS_LOG_UNCOND("Real-time simulator enabled (HardLimit mode, 100ms limit)");
+                           TimeValue(MilliSeconds(200)));
+        NS_LOG_UNCOND("Real-time simulator enabled (HardLimit mode, 200ms limit)");
     }
 
     // Update global handover interrupt parameters
